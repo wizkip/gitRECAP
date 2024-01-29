3244da31a40fa9bc33fa6b0ddd079143c8bb19c6
wizkip <kipkemoiwesley361@gmail.com>
2024-01-29 09:28:44 +0300

    A few files here

---
+++ sang.py
@@ -0,0 +1,25 @@
+#LETS GOOOOOOO
+
+
+from itertools import permutations
+
+#lets define a simple function
+
+def sang(str):
+
+    #create a LIST
+    permlist = permutations(str)
+
+    #Iterate through this list
+
+    for perm in list(permlist):
+
+        print(''.join(perm))
+
+
+if __name__ == "__main__":
+
+    str = "SANG"
+    sang(str)
+
+    
